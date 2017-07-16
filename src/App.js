2be4f603f389b8e7c5023623346b9f6191c3a4e4
flowerbot <flowerbot@gmail.com>
2017-07-16 12:10:51 +1000

    build renamed to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,57 +1,115 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import ListBooks from './ListBooks'
+import SearchBooks from './SearchBooks'
 import './App.css'
 import * as BooksAPI from './BooksAPI'
 
 
 class BooksApp extends Component {
 
-  state={
-    books: []
+  state = {
+    books: [],
+    foundbooks: []
   }
 
   componentDidMount() {
+    this.getBooks()
+  }
+
+  getBooks = () => {
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
     })
   }
 
-  removeBook = (book) => {
-    this.setState((state) => ({
-      books: state.books.filter((b) => b.id !== book.id)
-    }))
-  }
-
   moveBook = (book, shelf) => {
     this.setState(state => {
-        book.shelf = shelf
-        state.books.concat([ book ])
-      })
-      BooksAPI.update(book,shelf)
+      book.shelf = shelf
+      state.books.concat([book])
+    })
+    BooksAPI.update(book, shelf).then(this.getBooks)
+  }
+
+  findBook = (query, maxResults) => {
+    this.setState(state => {
+      if (query !== null || query.trim() !== '') {
+        BooksAPI.search(query, maxResults).then(books => {
+          for (const book of books) {
+            book.shelf = "none"
+            console.log(book)
+          }
+          this.setState({ foundbooks: books })
+        })
+      } else {
+        this.setState({ foundbooks: [] })
+      }
+    })
   }
 
   render() {
-    return (
-      <div className="BooksApp">
-      <ListBooks
-        shelfTitle='Currently Reading'
-        onDeleteBook={this.removeBook}
-        onMoveBook={this.moveBook}
-        booklist={this.state.books.filter((books) => books.shelf === 'currentlyReading')}
+    return ( <div className = "BooksApp">
+      <h1 className = "list-books-title"> My Reads </h1>
+      <Route path = "/search"
+        render = {() => (
+           <div>
+            <div className = "search-books-bar">
+              <Link className = "close-search" to = "/" />
+              <div className = "search-books-input-wrapper">
+                <input
+                  className = "search-books-bar"
+                  type = "text"
+                  placeholder = "Search books"
+                  onChange = {(event) => this.findBook(event.target.value, 5)}
+                />
+              </div>
+            </div>
+            <div>
+
+            <SearchBooks shelfTitle = "Search Results"
+              onMoveBook = { this.moveBook }
+              searchlist = { this.state.foundbooks.filter((books) => books.shelf === 'none')}
+            />
+          </div>
+          </div>
+          )
+        }
       />
-      <ListBooks
-        shelfTitle='Want To Read'
-        onDeleteBook={this.removeBook}
-        onMoveBook={this.moveBook}
-        booklist={this.state.books.filter((books) => books.shelf === 'wantToRead')}
+
+      <Route path = "/"
+        render = {() => (
+            <div>
+              <div className = "open-search">
+              <Link to = "/search"
+                onClick = { this.getBooks }>
+              </Link>
+              </div>
+              <ListBooks
+                shelfTitle = "Currently Reading"
+                onDeleteBook = { this.removeBook }
+                onMoveBook = { this.moveBook }
+                booklist = { this.state.books.filter((books) => books.shelf === 'currentlyReading') }
+              />
+              <ListBooks
+                shelfTitle = "Want To Read"
+                onDeleteBook = { this.removeBook }
+                onMoveBook = { this.moveBook }
+                booklist = { this.state.books.filter((books) => books.shelf === 'wantToRead') }
+              />
+              <ListBooks
+                shelfTitle = "Read"
+                onDeleteBook = { this.removeBook }
+                onMoveBook = { this.moveBook }
+                booklist = { this.state.books.filter((books) => books.shelf === 'read') }
+              />
+            </div>
+          )
+        }
       />
-      <ListBooks
-        shelfTitle='Read'
-        onDeleteBook={this.removeBook}
-        onMoveBook={this.moveBook}
-        booklist={this.state.books.filter((books) => books.shelf === 'read')}
-      />
-       </div>
+
+    </div>
+
     )
   }
 }
